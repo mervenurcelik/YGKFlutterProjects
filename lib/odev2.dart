9a6e98ac1ff637d9a36f7c1a4c2dda4241da8c66
mmerv <mmervenurcelik@gmail.com>
2024-08-27 12:30:24 +0300

    Odev-2-1&2&3

--- lib/odev2.dart
+++ lib/odev2.dart
@@ -19,9 +19,20 @@ void main(){
 
   for(int i =0; i < numbers.length; i++){
     numbersSums = numbers[i] + numbersSums;
-    print("i\'nin değeri : $i ");
-    print('Listenin şu anki değeri : ' + numbers[i].toString());
+    //print("i\'nin değeri : $i ");
+    //print('Listenin şu anki değeri : ' + numbers[i].toString());
   }
   print("Sayıların toplamı: " + numbersSums.toString());
+
+
+  // Question 3 : is there an Avatar movie?
+
+  Map filmler= { 'Inception': 2010, 'The Shawshank Redemption': 1994, 'The Godfather': 1972, 'Pulp Fiction': 1994, 'The Dark Knight': 2008, 'Fight Club': 1999, 'Forrest Gump': 1994, 'The Matrix': 1999, 'Goodfellas': 1990, 'The Lord of the Rings: The Return of the King': 2003, 'The Silence of the Lambs': 1991, 'Schindler\'s List': 1993, 'Gladiator': 2000, 'The Prestige': 2006, 'The Lion King': 1994, 'Saving Private Ryan': 1998, 'Jurassic Park': 1993, 'Back to the Future': 1985, 'Titanic': 1997, 'Avatar': 2009, };
+
+  if(filmler.containsKey('Avatar')){
+    print("There's the movie Avatar.");
+  } else {
+    print("No Avatar movie.");
+  }
 
 }
